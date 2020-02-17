67db970cc6149fe0d175e7d278f78cb25979d367
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-02-17 02:06:05 -0500

    initial commit with env, django installed, django startproject blog->folder renamed src

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/Swotty/Documents/justdjango-yt-advanced-blog-tutorial-with-extra-functionality/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
